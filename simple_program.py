890592246c671b32addd29d19fab089829b5bce0
AlbusNotThuan <107522443+AlbusNotThuan@users.noreply.github.com>
2024-11-21 19:00:01 +0700

    sua code

--- simple_program.py
+++ simple_program.py
@@ -4,3 +4,6 @@ def count_elements(array):
 # Example usage
 array = [1, 2, 3, 4, 5]
 print("Number of elements in the array:", count_elements(array))
+
+alist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Number of elements in the array:", count_elements(alist))
